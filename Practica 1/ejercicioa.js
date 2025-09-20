a1d37d21e409783a5fbec759b7da92fa1e5085da
trejofran <124050570@upq.edu.mx>
2025-09-19 20:20:20 -0600

    ejercicio a

---
+++ Practica 1/ejercicioa.js
@@ -0,0 +1,7 @@
+//Ejercicio A,
+let nombre = "Armando";
+const edad = 25;
+nombre= "Ana Maria";
+const saludo = "Hola " + nombre + " tienes " + edad + " años";
+//muestra en consola el contenido de saludo
+console.log(saludo);
