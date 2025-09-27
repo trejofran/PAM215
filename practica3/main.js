8524fee074e971031b5afa00f308ed77ac18d124
trejofran <124050570@upq.edu.mx>
2025-09-26 20:34:41 -0600

    Ejercicio 1
    uso de import y export para pasar fiuncion restar de util a main.js

---
+++ practica3/main.js
@@ -0,0 +1,4 @@
+import restar from './util.js';
+console.log("resultado", restar(10,5));
+console.log(restar(5,3));
+console.log(restar(2,1));
---
+++ practica3/util.js
@@ -0,0 +1,3 @@
+export default function restar (a,b){
+    return a - b;
+}
