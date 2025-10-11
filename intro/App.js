0ef4451af288986c4e4c4ce960ad833e0f77d65d
trejofran <124050570@upq.edu.mx>
2025-10-10 20:20:57 -0600

    Creacion de botones
    Creacion de botones y funciones de incrementar decrementar y reiniciar un contador en react con js

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,22 @@
+// zona 1 imports: zona de importaciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View,Button } from 'react-native';
+import React, {useState} from 'react';
+// zona 2 Main: zona de componentes
 export default function App() {
+  const[contador,setContador]=useState(0);
   return (
     <View style={styles.container}>
-      <Text>Hola mundo</Text>
+      <Text>Contador:{contador}</Text>
+      <Button title= "Agregar" onPress={()=>setContador(contador+1)}/>
+      <Button title= "Quitar" onPress={()=>setContador(contador-1)}/>  
+      <Button title= "Reiniciar" onPress={()=>setContador(0)}/>
+      <Button title = 'incrementar' onPress={()=>setContador(contador===0)}/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+//zona 3 styles: zona de estilos y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
