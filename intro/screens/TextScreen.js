bc74042f42482598e86f0fc33f3d2827635ba776
trejofran <124050570@upq.edu.mx>
2025-10-20 20:23:57 -0600

    text input y alert

--- intro/screens/TextScreen.js
+++ intro/screens/TextScreen.js
@@ -1,16 +1,115 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, Button, TextInput, Alert } from 'react-native'
+import React,{useState} from 'react';
 
 
-export default function TextScreen() {
 
-  
-    return (
-      <View>
-        <Text>Proximamente por Dulce</Text>
-      </View>
-    )
+
+export default function TextInputAlertScreen() {
+  const [nombre, setNombre]= useState('');
+  const [password, setPassword]= useState('');
+  const [Telefono, setTelefono]= useState('');
+
+  const mostrarAlerta = () => {
+     if(nombre.trim() === '' || password.trim() === '' || Telefono.trim() === '') {
+        Alert.alert("Error", "Por favor completa todos los campos (movile)");
+        alert("Error", "Por favor completa todos los campos (web)");
+        return;
+     } else {
+        
+        Alert.alert("Datos Ingresados \n" +
+        `Nombre: ${nombre}\n Password: ${password}
+        \nTelefono: ${Telefono}`
+        );
+        alert("Datos Ingresados \n" +
+        `Nombre: ${nombre}\nPassword: ${password}
+         \nTelefono: ${Telefono}`
+        );
+        
+
+
+
+
+
+
+     }
   }
 
+    return (
 
-const styles = StyleSheet.create({})
+      <View style={styles.container}>
 
+        <Text style= {styles.titulo}>TextInput & Alert</Text>
+        <Text style={styles.etiquetas}>Nombre:</Text>
+        <TextInput 
+            style={styles.input}
+           placeholder='Escribe tu nombre aquí'
+           value={nombre}
+           onChangeText={setNombre}
+        
+        />
+
+         <Text style= {styles.etiquetas}>Password:</Text>
+        <TextInput style={styles.input}
+           placeholder='Escribe tu password aquí'
+            secureTextEntry={true}
+           value={password}
+           onChangeText={setPassword}
+        
+        />
+
+         <Text>Telefono:</Text>
+        <TextInput style={styles.input}
+           placeholder='Escribe tu telefono aquí'
+           keyboardType='phone-pad'
+           value={Telefono}
+           onChangeText={setTelefono}
+        />
+        <Button
+        color="#841584"
+          title='Mostrar Alerta'
+           onPress={mostrarAlerta}
+        
+        />
+
+        
+        
+
+
+
+        
+      </View>
+    )
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#b9a7a7ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulo:{ 
+    fontFamily:'Arial',
+    fontSize:30,
+    color:'white',
+    marginBottom:20,
+    textDecorationLine:'underline',
+    fontWeight:'bold',
+  },
+  etiquetas:{
+    fontSize:18,
+    marginBottom:5,
+    marginTop:10,
+  },
+     input: {
+      width: '50%',
+      borderColor: 'gray',
+      borderWidth: 1,
+      padding: 10,
+      marginBottom: 15,
+      backgroundColor:'white',
+      borderRadius:5,
+
+     },
+})
