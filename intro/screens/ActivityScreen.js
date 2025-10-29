9b934391dca754593c6813320d3fd258b6ed903c
trejofran <124050570@upq.edu.mx>
2025-10-28 20:30:00 -0600

    activitycompletado

--- intro/screens/ActivityScreen.js
+++ intro/screens/ActivityScreen.js
@@ -1,15 +1,52 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, ActivityIndicator, useState } from 'react-native'
 
 
-export default function IndicatorScreen() {
+export default function ActivityIndicatorScreen() {
+  const [loading, setLoading]=useState(false);
+
+  const startLoading=()=>{
+    setLoading(true);
+    setTimeout(()=>setLoading(false),3000)
+  };
 
   
+    if (loading) {
+      return(
+      <view>
+        <view>
+          <ActivityIndicator
+          size='large'
+          color='#0000ff'
+          animating={true}
+          hidesWhenStopped={true}
+          />
+          <Text>Cargando....</Text>
+        </view>
+      </view>
+      );
+    }
     return (
       <View>
-        <Text>Proximamente por Dulce </Text>
+        <Text>ActivityIndicator</Text>
+        <Button title="Carga de datos" onPress= {startLoading}/>
       </View>
     )
+
+
+        
+      
+      
+    }
+
+  
+
+
+const styles = StyleSheet.create({
+
+  container :{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+
   }
-
-
-const styles = StyleSheet.create({})
+})
